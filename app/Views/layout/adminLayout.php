3db1fa6051fd6c17df10169b32fa4d73ad345de0
riyant <anggurryn106@gmail.com>
2023-10-22 18:26:00 +0700

    setup admin & auth view

--- app/Views/layout/adminLayout.php
+++ app/Views/layout/adminLayout.php
@@ -72,7 +72,7 @@
         <li class="nav-item">
           <a class="nav-link text-white " href="<?= base_url('auth/sign-up'); ?>">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">register</i>
+              <i class="material-icons opacity-10">assignment</i>
             </div>
             <span class="nav-link-text ms-1">Sign Up</span>
           </a>
--- app/Views/layout/authLayout.php
+++ app/Views/layout/authLayout.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,7 +29,7 @@
         <!-- Navbar -->
         <nav class="navbar navbar-expand-lg blur border-radius-xl top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
           <div class="container-fluid ps-2 pe-0">
-            <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="../pages/dashboard.html">
+            <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="<?= base_url(); ?>">
               Temansejalan.co
             </a>
             <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
